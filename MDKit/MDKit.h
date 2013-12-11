f81a8d37f16f26d1ce3b9e40295e12bb0a6b64ec
Matthew Dicembrino <matt@sixsidedstudio.com>
2013-12-11 02:45:35 -0500

    Initial Commit

---
+++ MDKit/MDKit.h
@@ -0,0 +1,13 @@
+//
+//  MDKit.h
+//  MDKit
+//
+//  Created by Matthew Dicembrino on 12/11/13.
+//  Copyright (c) 2013 Six Sided Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MDKit : NSObject
+
+@end
---
+++ MDKit/MDKit.m
@@ -0,0 +1,13 @@
+//
+//  MDKit.m
+//  MDKit
+//
+//  Created by Matthew Dicembrino on 12/11/13.
+//  Copyright (c) 2013 Six Sided Studio. All rights reserved.
+//
+
+#import "MDKit.h"
+
+@implementation MDKit
+
+@end
